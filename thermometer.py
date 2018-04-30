053153000e3029d4a9ccc30f1e8e15330b84e9ac
Zach Larson <zlarson@gmail.com>
2018-04-29 21:15:42 -0500

    added peewee

--- thermometer.py
+++ thermometer.py
@@ -1,6 +1,10 @@
+ 
 import os
 import glob
 import time
+import peewee
+from peewee import *
+import datetime
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -8,6 +12,15 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+db = MySQLDatabase('Greenhouse', user='greenhouseuser', passwd='1greenhouse!', port=3306)
+
+class TemperatureReading(peewee.Model):
+    internal_temperature = peewee.DecimalField()
+    created_date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -25,9 +38,13 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
-	
-while True:
-	print(read_temp())	
-	time.sleep(1)
 
+        TemperatureReading.create_table()
+        temp = TemperatureReading(internal_temperature=temp_f)
+        temp.save()
+
+        return temp_c, temp_f
+
+while True:
+        print(read_temp())
+        time.sleep(1)
